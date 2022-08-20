9c02f4c03355d63ff09ce27a95fa95c8cc1f84bf
Nay1997 <32686354+Najwax43@users.noreply.github.com>
2022-08-20 18:11:52 +0300

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+var myAge=21
+var currentTemperature = -1
+var size = 5.10
+
+
+var pi = 3.14
+var intPi = Int(pi)
+print(pi)
+print(intPi)
+
+
+var name = " Ahmed"
+print(name)
+name = " my name is  " + name
+print(name)
+
+var myLetter : Character = "N"
+
+
+var isSunny = true
+
+var currentScore = 90
+var hight: Float = 3.14
+
+
+
+
+var numberOne = 4.99
+var numberTwo = 5
+var numberThree = numberOne + numberTwo
+print(numberOne, numberTwo)
+
