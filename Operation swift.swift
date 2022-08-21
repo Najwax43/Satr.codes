6bdf867eaed72026730b2f7b605aceb8d2b4ee32
Nay1997 <32686354+Najwax43@users.noreply.github.com>
2022-08-21 17:36:26 +0300

    Add files via upload

---
+++ Operation swift.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+var result = 5+2
+print(result)
+
+
+
+let value1 = true
+let value2 = false
+print(value1 && value2)
+print(value1 || value2)
+
+
+print (5>1)
+
+var result = 6
+result-=2
+print(result)
+
+
+
+let x = 3
+let y = 3
+let z = 10
+let resultOne = x != y
+let resultTwo = z > x
+let logicalResult = resultOne || resultTwo
+
+
