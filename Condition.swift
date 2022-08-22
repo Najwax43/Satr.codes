22752daf57115fc3a92b0af48adac1b966953686
Nay1997 <32686354+Najwax43@users.noreply.github.com>
2022-08-22 16:36:36 +0300

    Add files via upload

---
+++ Condition.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+let age = 20
+
+
+if age < 12 {
+    print("sorry, you are not old enouph to play this game")
+
+}
+else {
+    
+print("Welcome to the game")
+}
+
+
+
+
+let colors = [ "red","green","blue"]
+let colorNumber = 20
+
+switch colorNumber {
+case 0:
+    print(colors[0])
+case 1:
+    print(colors[1])
+case 2:
+    print(colors[2])
+    
+default:
+    print("White")
+}
